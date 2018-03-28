b5cd58cf3f1ca0a0f3cbcf7b089542883dd4a113
yangdaolong8 <yangdaolong8@163.com>
2018-03-28 16:41:55 +0800

    new class

---
+++ StrictModeDemo/src/main/java/com/example/strictmode/FirstActivity.java
@@ -0,0 +1,16 @@
+package com.example.strictmode;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 11234567 on 2018/3/28.
+ */
+
+public class FirstActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_first);
+    }
+}
